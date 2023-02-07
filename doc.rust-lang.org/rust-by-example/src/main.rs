2ab45d6910af9677177aed1e6f0cd08b327299b5
linnefromice <linnefromice@gmail.com>
2023-02-07 13:21:20 +0900

    update: hello world - display

--- doc.rust-lang.org/rust-by-example/src/main.rs
+++ doc.rust-lang.org/rust-by-example/src/main.rs
@@ -1,10 +1,19 @@
+use std::fmt;
+
 #[derive(Debug)]
 struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({} {})", self.0, self.1)
+    }
+}
 
 fn main_display() {
     let minmax = MinMax(0, 14);
 
     println!("Compare structures");
+    println!("Display: {}", minmax);
     println!("Debug: {:?}", minmax);
 }
 
