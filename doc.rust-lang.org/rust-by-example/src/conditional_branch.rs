21164ac9fc10cbb6879aed2bdfb2dd459f3c15a8
linnefromice <linnefromice@gmail.com>
2023-02-08 11:54:49 +0900

    update: conditional branch - match

--- doc.rust-lang.org/rust-by-example/src/conditional_branch.rs
+++ doc.rust-lang.org/rust-by-example/src/conditional_branch.rs
@@ -12,3 +12,23 @@ pub fn execute_conditional_branch() {
     }
     println!("Exited the outer loop");
 }
+
+fn execute_simple_match(num: usize, boolean: bool) {
+    println!("Tell me about {}", num);
+    match num {
+        1 => println!("One!"),
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        13..=19 => println!("A teen"),
+        _ => println!("Ain't special"),
+    }
+
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    };
+    println!("{} -> {}", boolean, binary);
+}
+
+pub fn execute_conditional_branch_match() {
+    execute_simple_match(13, true);
+}
--- doc.rust-lang.org/rust-by-example/src/main.rs
+++ doc.rust-lang.org/rust-by-example/src/main.rs
@@ -1,5 +1,5 @@
-use rust_by_example::conditional_branch::execute_conditional_branch;
+use rust_by_example::conditional_branch::execute_conditional_branch_match;
 
 fn main() {
-    execute_conditional_branch();
+    execute_conditional_branch_match();
 }
