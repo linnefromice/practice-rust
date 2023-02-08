3b07582441d58dbf9b0af26ecfdd96ef44e576fb
linnefromice <linnefromice@gmail.com>
2023-02-08 12:01:28 +0900

    update: conditional branch - match (tuple)

--- doc.rust-lang.org/rust-by-example/src/conditional_branch.rs
+++ doc.rust-lang.org/rust-by-example/src/conditional_branch.rs
@@ -29,6 +29,18 @@ fn execute_simple_match(num: usize, boolean: bool) {
     println!("{} -> {}", boolean, binary);
 }
 
+fn execute_match_destruct_tuple(triple: (i32, i32, i32)) {
+    println!("Tell me about {:?}", triple);
+
+    match triple {
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..) => println!("First is `1`, and the rest doesn't matter"),
+        (2, ..) => println!("First is `2`, and the rest doesn't matter"),
+        (3, .., 4) => println!("First is `3`, last is `4` and the rest doesn't matter"),
+        _ => println!("It doesn't matter what they are"),
+    }
+}
+
 pub fn execute_conditional_branch_match() {
-    execute_simple_match(13, true);
+    execute_match_destruct_tuple((0, -2, 3));
 }
