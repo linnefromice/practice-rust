86ad8f6f1d6a8acb82fc2f6380d4f4b4752b9c07
linnefromice <linnefromice@gmail.com>
2023-02-15 17:12:47 +0900

    update: todo cli - add func (list tasks)

--- learn.microsoft.com/rusty-journal/src/tasks.rs
+++ learn.microsoft.com/rusty-journal/src/tasks.rs
@@ -1,9 +1,9 @@
 use std::{
     path::PathBuf,
-    fs::{OpenOptions},
-    io::{Seek, SeekFrom, Error, ErrorKind, self}
+    fs::{OpenOptions, File},
+    io::{Seek, SeekFrom, Error, ErrorKind, self}, fmt
 };
-use chrono::{DateTime, Utc, serde::ts_seconds};
+use chrono::{DateTime, Utc, serde::ts_seconds, Local};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -20,6 +20,24 @@ impl Task {
     }
 }
 
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>, io::Error> {
+    file.seek(SeekFrom::Start(0))?; // Rewind the file before
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?; // Rewind the file after
+    Ok(tasks)
+}
+
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<(), io::Error> {
     // Open the file.
     let mut file = OpenOptions::new()
@@ -29,14 +47,7 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<(), io::Error> {
         .open(journal_path)?;
 
     // Consume the file's contents as a vector of tasks.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
-
-    // Rewind the file after reading from it.
-    file.seek(SeekFrom::Start(0))?;
+    let mut tasks: Vec<Task> = collect_tasks(&file)?;
 
     // Write the modified task list back into the file.
     tasks.push(task);
@@ -53,11 +64,7 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<(), 
         .open(journal_path)?;
 
     // Consume the file's contents as a vector of tasks.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
+    let mut tasks: Vec<Task> = collect_tasks(&file)?;
 
     // Remove the task.
     if task_position == 0 || task_position > tasks.len() {
@@ -66,8 +73,27 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<(), 
     tasks.remove(task_position - 1);
 
     // Write the modified task list back into the file.
-    file.seek(SeekFrom::Start(0))?;
     file.set_len(0)?;
     serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<(), io::Error> {
+    // Open the file
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    // Parse the file and collect the tasks.
+    let tasks = collect_tasks(&file)?;
+
+    // Emurate and display tasks, if any.
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
     Ok(())
 }
