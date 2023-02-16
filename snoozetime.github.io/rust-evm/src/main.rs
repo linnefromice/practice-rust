6978541c0c3f2ea3359213014858db25ab347c3f
linnefromice <linnefromice@gmail.com>
2023-02-16 22:24:05 +0900

    update: rust-vm - decode

--- snoozetime.github.io/rust-evm/src/main.rs
+++ snoozetime.github.io/rust-evm/src/main.rs
@@ -7,6 +7,17 @@ fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
         .collect()
 }
 
+#[derive(Debug)]
+enum Opcode {
+    STOP, // 0x00
+    ADD, // 0x01
+    MUL, // 0x02
+    PUSH1(u8), // 0x60
+    PUSH2(u8, u8), // 0x61
+    // PUSH32(u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8) // 0x7f
+    EOF,
+}
+
 struct Vm {
     code: Vec<u8>, // smart contract code
     pc: usize,
@@ -20,15 +31,51 @@ impl Vm {
         let code = decode(&buffer).unwrap();
         Ok(Vm { code, pc: 0 })
     }
-}
 
+    fn next(&mut self) -> Option<Opcode> {
+        if self.pc >= self.code.len() {
+            return Some(Opcode::EOF);
+        }
+
+        match self.code[self.pc] {
+            0x00 => {
+                self.pc += 1;
+                Some(Opcode::STOP)
+            },
+            0x01 => {
+                self.pc += 1;
+                Some(Opcode::ADD)
+            },
+            0x02 => {
+                self.pc += 1;
+                Some(Opcode::MUL)
+            },
+            0x60 => {
+                let value = self.code[self.pc+1];
+                self.pc += 2;
+                Some(Opcode::PUSH1(value))
+            },
+            0x61 => {
+                let value0 = self.code[self.pc+1];
+                let value1 = self.code[self.pc+2];
+                self.pc += 3;
+                Some(Opcode::PUSH2(value0, value1))
+            },
+            _ => { self.pc += 1; None }
+        }
+    }
+}
 
 fn run() -> Result<(), Error> {
     let filename = "bytecode-addtion";
-    let vm = Vm::new_from_file(&filename).unwrap();
+    let mut vm = Vm::new_from_file(&filename).unwrap();
 
-    for b in &vm.code {
-        println!("0x{:x}", b);
+    loop {
+        match vm.next() {
+            Some(Opcode::EOF) => break,
+            Some(x) => println!("{:?}", x),
+            None => {}
+        }
     }
 
     Ok(())
