2c56a835a4dd22c09d20b3da60cf239475069738
linnefromice <linnefromice@gmail.com>
2023-02-16 14:07:33 +0900

    update: actix tutorial - extractor (use serde & query)

--- actix.rs/basics-extractors-handlers/src/main.rs
+++ actix.rs/basics-extractors-handlers/src/main.rs
@@ -1,14 +1,30 @@
 use actix_web::{HttpServer, App, web};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+struct UsersInfo {
+    user_id: u32,
+    friend: String
+}
 #[actix_web::get("/users/{user_id}/{friend}")]
-async fn index(path: web::Path<(u32, String)>) -> std::io::Result<String> {
-    let (user_id, friend) = path.into_inner();
-    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+async fn users(info: web::Path<UsersInfo>) -> std::io::Result<String> {
+    Ok(format!("Welcome {}, user_id {}!", info.friend, info.user_id))
+}
+
+#[derive(Deserialize)]
+struct IndexInfo {
+    username: String,
+}
+#[actix_web::get("/")]
+async fn index(info: web::Query<IndexInfo>) -> String {
+    format!("Welcome {}!", info.username)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new()
+            .service(index)
+            .service(users))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
