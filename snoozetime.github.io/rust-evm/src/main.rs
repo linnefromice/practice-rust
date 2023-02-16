487f865da7961e579dc7a503af35d601258913d9
linnefromice <linnefromice@gmail.com>
2023-02-16 21:52:08 +0900

    update

--- snoozetime.github.io/rust-evm/src/main.rs
+++ snoozetime.github.io/rust-evm/src/main.rs
@@ -1,12 +1,36 @@
-use std::{io::{Error, Read}, fs::File};
+use std::{io::{Error, Read}, fs::File, num::ParseIntError};
+
+fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..(s.len()-1))
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i+2], 16))
+        .collect()
+}
+
+struct Vm {
+    code: Vec<u8>, // smart contract code
+    pc: usize,
+}
+
+impl Vm {
+    fn new_from_file(filename: &str) -> Result<Vm, Error> {
+        let mut f = File::open(filename)?;
+        let mut buffer = String::new();
+        f.read_to_string(&mut buffer)?;
+        let code = decode(&buffer).unwrap();
+        Ok(Vm { code, pc: 0 })
+    }
+}
+
 
 fn run() -> Result<(), Error> {
     let filename = "bytecode-addtion";
-    let mut f = File::open(filename)?;
-    let mut buffer = String::new();
-    f.read_to_string(&mut buffer)?;
+    let vm = Vm::new_from_file(&filename).unwrap();
 
-    println!("{}", buffer);
+    for b in &vm.code {
+        println!("0x{:x}", b);
+    }
+
     Ok(())
 }
 
