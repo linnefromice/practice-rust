f23476b3f4289ab5b2ab3769cb46b2a15cf78dbe
linnefromice <linnefromice@gmail.com>
2023-02-17 11:26:06 +0900

    update: rust-vm - to cli

--- snoozetime.github.io/rust-evm/src/main.rs
+++ snoozetime.github.io/rust-evm/src/main.rs
@@ -1,13 +1,21 @@
-use std::io::Error;
+use std::{io::Error, env};
 
 use rust_evm::vm::{Vm, self};
 
 fn run() -> Result<(), Error> {
-    let filename = "bytecode-add";
+    let args: Vec<String> = env::args().collect();
+    let function = args[1].clone();
+    let filename = if args.len() == 3 && !args[2].is_empty() { args[2].clone() } else { String::from("bytecode-add") };
+    println!("In file {}", filename);
+    
     let mut vm = Vm::new_from_file(&filename).unwrap();
+    println!("Correctly loaded VM");
 
-    // vm::debug(&mut vm);
-    vm::interpret(&mut vm);
+    match function.as_str() {
+        "debug" => vm::debug(&mut vm),
+        "run" => vm::interpret(&mut vm),
+        _ => panic!("Expect either 'debug' or 'run' for first parameter")
+    }
 
     Ok(())
 }
