6874f4dae2c494d73f4cf6d281172f0e30dab67c
linnefromice <linnefromice@gmail.com>
2023-02-28 13:43:03 +0900

    clean

--- ink-language/docs.substrate.io/mycontract/lib.rs
+++ ink-language/docs.substrate.io/mycontract/lib.rs
@@ -25,29 +25,4 @@ mod mycontract {
             self.my_map.get(&caller).unwrap_or_default()
         }
     }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let mycontract = Mycontract::default();
-            assert_eq!(mycontract.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut mycontract = Mycontract::new(false);
-            assert_eq!(mycontract.get(), false);
-            mycontract.flip();
-            assert_eq!(mycontract.get(), true);
-        }
-    }
 }
