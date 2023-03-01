782d46ac5b9ca917a266d6eeb6bb371708b6e16c
linnefromice <linnefromice@gmail.com>
2023-03-01 22:02:05 +0900

    update: stable_coin - use openbrush & remove template

--- ink-language/docs.openbrush.io/example_project_structure/contracts/stable_coin/lib.rs
+++ ink-language/docs.openbrush.io/example_project_structure/contracts/stable_coin/lib.rs
@@ -1,143 +1,46 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
 
-#[ink::contract]
-mod stable_coin {
+#[openbrush::contract]
+pub mod token {
+    // use monorepo_project::traits::stable_coin::*;
+    use openbrush::{
+        contracts::psp22::extensions::{
+            metadata::*,
+        },
+        traits::{Storage, String},
+    };
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct StableCoin {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    #[derive(Default, Storage)]
+    pub struct StableCoinContract {
+        #[storage_field]
+        psp22: psp22::Data,
+        #[storage_field]
+        metadata: metadata::Data,
     }
 
-    impl StableCoin {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    impl StableCoinContract {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(
+            name: Option<String>,
+            symbol: Option<String>,
+            decimals: u8,
+        ) -> Self {
+            Self {
+                psp22: psp22::Data::default(),
+                metadata: metadata::Data {
+                    name,
+                    symbol,
+                    decimals,
+                    _reserved: None,
+                },
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let stable_coin = StableCoin::default();
-            assert_eq!(stable_coin.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut stable_coin = StableCoin::new(false);
-            assert_eq!(stable_coin.get(), false);
-            stable_coin.flip();
-            assert_eq!(stable_coin.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = StableCoinRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("stable_coin", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<StableCoinRef>(contract_account_id.clone())
-                .call(|stable_coin| stable_coin.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = StableCoinRef::new(false);
-            let contract_account_id = client
-                .instantiate("stable_coin", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<StableCoinRef>(contract_account_id.clone())
-                .call(|stable_coin| stable_coin.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<StableCoinRef>(contract_account_id.clone())
-                .call(|stable_coin| stable_coin.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<StableCoinRef>(contract_account_id.clone())
-                .call(|stable_coin| stable_coin.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+        pub fn decimals(&self) -> u8 {
+            self.metadata.token_decimals()
         }
     }
 }
