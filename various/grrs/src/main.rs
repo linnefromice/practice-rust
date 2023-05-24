9277edc8870709646128c2738acdc360dc18cdc2
linnefromice <linnefromice@gmail.com>
2023-05-25 00:27:54 +0900

    update

--- various/grrs/src/main.rs
+++ various/grrs/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
@@ -6,14 +7,32 @@ struct Cli {
     path: std::path::PathBuf
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line).expect("could not write line");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_a_match() {
+        let mut result = Vec::new();
+        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+        assert_eq!(result, b"lorem ipsum\n");
     }
 }
