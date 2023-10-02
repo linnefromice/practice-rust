66aefcdd7b6ab9004d175d484fa2ca350f5ba510
linnefromice <linnefromice@gmail.com>
2023-10-02 16:43:29 +0900

    update

--- hello_log/src/main.rs
+++ hello_log/src/main.rs
@@ -1,3 +1,20 @@
+use slog::{Level, Drain, Logger};
+
 fn main() {
-    println!("Hello, world!");
+    let log_level = Level::Trace;
+
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog::LevelFilter::new(drain, log_level).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let logger = Logger::root(drain, slog::o!());
+
+    // Log a trace-level message
+    slog::trace!(logger, "This is a trace message");
+
+    // Log messages at other log levels
+    slog::info!(logger, "This is an info message");
+    slog::warn!(logger, "This is a warning message");
+    slog::error!(logger, "This is an error message");
 }
