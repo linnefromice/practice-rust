e9f8695b0128b4ddeeb6840811c26e9ce52b0dbd
linnefromice <linnefromice@gmail.com>
2023-10-05 13:13:00 +0900

    add: clap

--- ws-interactive/by-dialoguer/src/main.rs
+++ ws-interactive/by-dialoguer/src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    operation: Operation
+}
+
+#[derive(Subcommand)]
+enum Operation {
+    Do,
+    Did,
+    Doing,
+    Done,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
+    match &cli.operation {
+        Operation::Do => println!("You do it!"),
+        Operation::Did => println!("You did it."),
+        Operation::Doing => println!("You doing it now!"),
+        Operation::Done => println!("You have done it..."),
+    }
 }
