11fe98d0843e652068e413b34351be5e04a2412a
linnefromice <linnefromice@gmail.com>
2023-10-06 22:13:57 +0900

    update

--- various/test_snapshot_1/src/main.rs
+++ various/test_snapshot_1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    #[test]
+    fn it_works() {
+        let actual = r#"fn main() {
+    println!("Hello, world!");
+    println!("Hello, sample_code!");
+}"#;
+        let expected = fs::read_to_string("assets/sample_code.rs").unwrap();
+
+        assert_eq!(actual, expected);
+    }
+}
