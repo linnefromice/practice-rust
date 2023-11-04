cd688e4a4eff3cf26db17ba2e97b155fef16ad40
linnefromice <linnefromice@gmail.com>
2023-11-04 17:42:55 +0900

    update: use json-typegen

--- various/json-typegen-01/src/main.rs
+++ various/json-typegen-01/src/main.rs
@@ -1,3 +1,30 @@
+use json_typegen::json_typegen;
+
+json_typegen!("User", r#"
+{
+    "name": "John Doe",
+    "age": 43,
+    "phones": [
+        "+44 1234567",
+        "+44 2345678"
+    ]
+}"#);
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        name: "John Doe".to_string(),
+        age: 43,
+        phones: vec!["+44 1234567".to_string(), "+44 2345678".to_string()]
+    };
+
+    // let user = (r#"
+    // {
+    //     "name": "John Doe",
+    //     "age": 43,
+    //     "phones": [
+    //         "+44 1234567",
+    //         "+44 2345678"
+    //     ]
+    // }"#).unwrap();
+    dbg!(user);
 }
