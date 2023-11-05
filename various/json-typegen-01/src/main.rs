b6f7f7e9394d0ec219ef9e452e334b018352cacd
linnefromice <linnefromice@gmail.com>
2023-11-05 15:13:32 +0900

    update

--- various/json-typegen-01/src/main.rs
+++ various/json-typegen-01/src/main.rs
@@ -1,30 +1,41 @@
-use json_typegen::json_typegen;
 
-json_typegen!("User", r#"
-{
-    "name": "John Doe",
-    "age": 43,
-    "phones": [
-        "+44 1234567",
-        "+44 2345678"
-    ]
-}"#);
+// json_typegen!("SampleUser", r#"
+// {
+//     "name": "John Doe",
+//     "age": 43,
+//     "phones": [
+//         "+44 1234567",
+//         "+44 2345678"
+//     ]
+// }"#);
+
+use std::fs;
+
+use json_typegen_shared::{codegen, Options};
 
 fn main() {
-    let user = User {
-        name: "John Doe".to_string(),
-        age: 43,
-        phones: vec!["+44 1234567".to_string(), "+44 2345678".to_string()]
-    };
+    // let args: Vec<String> = std::env::args().collect();
+    // if args.len() != 2 {
+    //     println!("Usage: {} <json_file>", args[0]);
+    //     std::process::exit(1);
+    // }
 
-    // let user = (r#"
-    // {
-    //     "name": "John Doe",
-    //     "age": 43,
-    //     "phones": [
-    //         "+44 1234567",
-    //         "+44 2345678"
-    //     ]
-    // }"#).unwrap();
-    dbg!(user);
+    // let user = SampleUser {
+    //     name: "John Doe".to_string(),
+    //     age: 43,
+    //     phones: vec!["+44 1234567".to_string(), "+44 2345678".to_string()]
+    // };
+    // dbg!(user);
+
+    // let file = File::open("src/response.json").unwrap();
+    let response_str = fs::read_to_string("src/response.json").unwrap();
+    let generated = codegen("SampleUser", &response_str, Options::default());
+    match generated {
+        Ok(generated) => {
+            println!("{}", generated);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 }
