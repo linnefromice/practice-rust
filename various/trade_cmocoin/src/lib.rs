f84811d9318fc61a192802777427f83fd1b4702b
linnefromice <linnefromice@gmail.com>
2024-03-01 22:27:55 +0900

    update: status endpoint

--- various/trade_cmocoin/src/lib.rs
+++ various/trade_cmocoin/src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{de::DeserializeOwned, Deserialize};
+
+const PUBLIC_API: &str = "https://api.coin.z.com/public";
+const _PRIVATE_API: &str = "https://api.coin.z.com/private";
+
+#[derive(Debug)]
+struct Error(String);
+
+fn get<T: DeserializeOwned>(url: &str) -> Result<T, Error> {
+    let body = ureq::get(url).call().map_err(|e| Error(e.to_string()))?;
+    body.into_json().map_err(|e| Error(e.to_string()))
+}
+
+#[derive(Debug, Deserialize)]
+struct StatusResponse {
+    data: StatusResponseStatus,
+    responsetime: String,
+    status: u8
+}
+#[derive(Debug, Deserialize)]
+struct StatusResponseStatus {
+    status: String,
+}
+pub fn public_status() -> Result<StatusResponse, Error> {
+    let url = format!("{}/v1/status", PUBLIC_API);
+    get(&url)
 }
 
 #[cfg(test)]
@@ -7,8 +31,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_public_status() {
+        let res = public_status();
+        assert!(res.is_ok());
+        let val = res.unwrap();
+        assert_eq!(val.status, 0);
     }
 }
